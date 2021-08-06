9bbeb4c3478f5966f3c7dba373f5cd7e36f2b0d4
Dmitriy Tarasevich <dtarasevich55@gmail.com>
2021-08-05 22:04:18 -0400

    Update API

--- app/src/main/java/com/png/interview/weather/api/WeatherApi.kt
+++ app/src/main/java/com/png/interview/weather/api/WeatherApi.kt
@@ -15,5 +15,5 @@ interface WeatherApi {
     suspend fun getAutocompleteResults(@Query("q") query: String): NetworkResponse<AutoCompleteResponse, Unit>
 
     @GET("forecast.json")
-    suspend fun getForecast(@Query("q") query: String): NetworkResponse<ForecastResponse, Unit>
+    suspend fun getForecast(@Query("q") query: String, @Query("days") days: Int = 10): NetworkResponse<ForecastResponse, Unit>
 }
