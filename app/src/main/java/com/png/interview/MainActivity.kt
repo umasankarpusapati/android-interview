c3d4542b98b45576ba980737e5c8edbb5b5fbec9
Ryan Hallermeier <TheHal85@gmail.com>
2021-08-06 00:22:05 -0400

    Cleanup after Refactor. Removed HotS references.

--- app/src/main/java/com/png/interview/MainActivity.kt
+++ app/src/main/java/com/png/interview/MainActivity.kt
@@ -8,26 +8,8 @@ import com.png.interview.ui.InjectedActivity
 
 class MainActivity : InjectedActivity() {
 
-    companion object {
-        val topLevelDestinations = setOf(
-            R.id.heroes_fragment,
-            R.id.maps_fragment,
-            R.id.about_fragment
-        )
-    }
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupNavigation()
     }
-
-    private fun setupNavigation() {
-
-    }
-
-    override fun onSupportNavigateUp() =
-        findNavController(R.id.mainNavigationFragment).navigateUp()
 }
